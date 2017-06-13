a83c6574f55ded3e7f71185891b7d360f09d8048
Jason Jones <jason@saxifrages.com>
2017-06-12 17:18:51 -0700

    Add new ability to process a string and return a simple map of parent to children

--- src/test/java/com/jasonwjones/hyperion/parentinferrer/ParentInferrerTest.java
+++ src/test/java/com/jasonwjones/hyperion/parentinferrer/ParentInferrerTest.java
@@ -1,6 +1,9 @@
 package com.jasonwjones.hyperion.parentinferrer;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -37,4 +40,27 @@ public class ParentInferrerTest {
 		String text = "   apple";
 		assertEquals("apple", ParentInferrer.textWithoutPrefix(text, ' '));
 	}
+	
+	@Test
+	public void testProcessString() {
+		String hier = "Time\n Qtr1\n Qtr2\n  Jan\n  Feb\n  Mar\n";
+		ParentInferrer pi = new ParentInferrer(new BasicOptions());
+		Map<String, List<String>> children = pi.processString(hier);
+		assertEquals(3, children.size());
+		assertEquals(2, children.get("Time").size());
+	}
+	
+	@Test
+	public void testProcessStringCreateChildEntriesWhenNoChildren() {
+		String hier = "Time\n Qtr1\n Qtr2\n  Jan\n  Feb\n  Mar\n";
+		BasicOptions options = new BasicOptions();
+		options.setCreateEntriesForChildless(true);
+		ParentInferrer pi = new ParentInferrer(options);
+		
+		Map<String, List<String>> children = pi.processString(hier);
+		assertEquals(7, children.size());
+		assertEquals(2, children.get("Time").size());
+		assertEquals(0, children.get("Jan").size());
+	}
+	
 }
